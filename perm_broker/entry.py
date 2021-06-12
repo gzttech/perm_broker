a3a06f5b8e2131bbca42792976e59f4c166b3d4d
socrateslee <socrateslee@users.noreply.github.com>
2021-06-12 16:41:23 +0800

    adjust the code to init perm_broker, allow separated init of db and flask blueprint

--- perm_broker/entry.py
+++ perm_broker/entry.py
@@ -2,45 +2,43 @@
 '''
 Basic root blueprint entry point.
 '''
-import flask
 from .common import conf, pb_root
 from .backend import init_db
-from . import default
 
 
-def setup_flask_session(flask_app):
-    session_db = mongoengine.connection.get_db(alias=default.MONGOENGINE_ALIAS)
-    config_dict = {
-        "SESSION_COOKIE_NAME": conf.SESSION_COOKIE_NAME,
-        "SESSION_TYPE": "mongodb",
-        "SESSION_MONGODB": session_db.client,
-        "SESSION_MONGODB_DB": session_db.name,
-        "SESSION_MONGODB_COLLECT": "sessions",
-        "PERMANENT_SESSION_LIFETIME": conf.PAULI_SESSION_LIFETIME
-    }
-    flask_app.config.update(config_dict)
-    flask_session.Session(flask_app)
-
-
-def setup_perm_broker(flask_app, url_prefix="/pb", **config):
-    # write config to conf
+def setup_conf(**config):
+    '''
+    write config to conf module
+    '''
     for k, v in config.items():
         setattr(conf, k, v)
 
-    # setup database
-    init_db(**config)
 
+def setup_flask(flask_app, url_prefix="/pb"):
+    '''
+    Setup flask, register perm_broker as a blueprint.
+    '''
     # import sub level path routings
-    from .user.views import api
-    from .auth.views import api
-    from .auth.views import web
-    from .perm.views import api
+    from .user.views import api as user_api
+    from .auth.views import api as auth_api
+    from .auth.views import web as auth_web
+    from .perm.views import api as perm_api
 
+    # enable debug login
     if conf.DEBUG and getattr(conf, 'ENABLE_DEBUG_LOGIN', False):
         from .auth.views import debug_api
 
-    # bind pauli blueprint
+    # bind perm broker blueprint
     flask_app.register_blueprint(pb_root, url_prefix=url_prefix)
 
 
-__ALL__ = ['pb_root', 'conf', 'setup_perm_broker']
+def setup_perm_broker(flask_app, url_prefix="/pb", **config):
+    '''
+    Setup perm broker.
+    '''
+    setup_conf(**config)
+    init_db(**config)
+    setup_flask(flask_app, url_prefix=url_prefix)
+
+
+__ALL__ = ['pb_root', 'conf', 'init_db', 'setup_perm_broker']
